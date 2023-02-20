217df413bd6cd3cd8e7ee25c9ccf6ed42af7c673
Sergio <afirthes@gmail.com>
2023-02-20 21:06:46 +0300

    Unit tests.

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -15,10 +15,13 @@ final class TipCalculatorTests: XCTestCase {
     private var sut: CalculatorVM!
     private var cancellables: Set<AnyCancellable>!
     
-    private let logoViewTapSubject = PassthroughSubject<Void, Never>()
+    private var logoViewTapSubject: PassthroughSubject<Void, Never>!
+    private var audioPlayerService: MockAudioPlayerService!
     
     override func setUp() {
-        sut = .init()
+        logoViewTapSubject = PassthroughSubject<Void, Never>()
+        audioPlayerService = MockAudioPlayerService()
+        sut = .init(audioPlayerService: audioPlayerService)
         cancellables = .init()
         super.setUp()
         
@@ -28,6 +31,8 @@ final class TipCalculatorTests: XCTestCase {
         super.tearDown()
         cancellables = nil
         sut = nil
+        audioPlayerService = nil
+        logoViewTapSubject = nil
     }
     
     func testResultWithoutTipFor1Person() {
@@ -50,6 +55,81 @@ final class TipCalculatorTests: XCTestCase {
         
     }
     
+    func testResultWithoutTipFor2erson() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
+        let output = sut.transform(input: input)
+        // then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith10PercentTipFor2Person() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
+        let output = sut.transform(input: input)
+        // then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipFor4Person() {
+        // given
+        let bill: Double = 200.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
+        let output = sut.transform(input: input)
+        // then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
+        }.store(in: &cancellables)
+    }
+    
+    func testSoundPlayedCalculatorResetOnLogoViewTap()  {
+        
+        //given
+        let input = buildInput(bill: 100, tip: .tenPercent, split: 2)
+        let output = sut.transform(input: input)
+        let expectation1 = XCTestExpectation(description: "reset calculator called")
+        let expectation2 = audioPlayerService.expectation
+        
+        //then
+        output.resetCalculatorPublisher.sink { _ in
+            expectation1.fulfill()
+        }.store(in: &cancellables)
+        
+        //when
+        logoViewTapSubject.send()
+        wait(for: [expectation1, expectation2], timeout: 1.0)
+        
+    }
     
     private func buildInput(
         bill: Double,
@@ -63,4 +143,13 @@ final class TipCalculatorTests: XCTestCase {
             logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher())
     }
     
+}
+
+class MockAudioPlayerService: AudioPlayerService {
+    
+    var expectation = XCTestExpectation(description: "playSound is called")
+    
+    func playSound() {
+        expectation.fulfill()
+    }
 }
